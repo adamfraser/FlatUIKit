bf51fe0322f428294f084d3839c57daa972f6a98
Adam Fraser <adamfraser0@gmail.com>
2013-06-09 18:14:39 +1000

    Add aditional FUISwitchTests

--- Tests/FUIKitTestsTests/FUISwitchTests.m
+++ Tests/FUIKitTestsTests/FUISwitchTests.m
@@ -13,6 +13,7 @@
 
 @interface FUISwitchTests : SenTestCase
 @property (strong, nonatomic) FUISwitch *fuiSwitch;
+@property (nonatomic) BOOL controlValueChangedToggle;
 @end
 
 @implementation FUISwitchTests
@@ -20,11 +21,32 @@
 - (void)setUp {
     [super setUp];
     self.fuiSwitch = [[FUISwitch alloc] initWithFrame:CGRectMake(0, 0, 80, 35)];
+    self.controlValueChangedToggle = NO;
 }
 
 - (void)testSwitchInitializesWithDefaultValues {
     expect(self.fuiSwitch.onLabel.text).to.equal(@"ON");
     expect(self.fuiSwitch.offLabel.text).to.equal(@"OFF");
+}
+
+- (void)testSwitchDefaultsOn {
+    expect(self.fuiSwitch.isOn).to.beTruthy();
+}
+
+- (void)testSetOnChangesValue {
+    expect(self.fuiSwitch.isOn).to.beTruthy();
+    [self.fuiSwitch setOn:NO animated:NO];
+    expect(self.fuiSwitch.isOn).to.beFalsy();
+}
+
+- (void)testUIControlEventValueChangedIsCalledWhenSwitchToggles {
+    [self.fuiSwitch addTarget:self action:@selector(switchValueChanged) forControlEvents:UIControlEventValueChanged];
+    [self.fuiSwitch setOn:NO animated:NO];
+    expect(self.controlValueChangedToggle).to.beTruthy();
+}
+
+- (void)switchValueChanged {
+    self.controlValueChangedToggle = YES;
 }
 
 @end
